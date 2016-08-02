5deeda43a857a8f4df942ebd3fae031c41ef51f6
Alex Sielicki <alex@techmilo.com>
2016-08-01 21:18:26 -0600

    Making interface more specific

--- src/plonetheme/popejoy2016/interfaces.py
+++ src/plonetheme/popejoy2016/interfaces.py
@@ -2,7 +2,8 @@
 """Module where all interfaces, events and exceptions live."""
 
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from plone.app.event.interfaces import IBrowserLayer
 
 
-class IPlonethemePopejoy2016Layer(IDefaultBrowserLayer):
+class IPlonethemePopejoy2016Layer(IBrowserLayer):
     """Marker interface that defines a browser layer."""
